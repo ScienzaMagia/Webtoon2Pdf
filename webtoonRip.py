992442e23a796b978e66887faa4ba874f19611ba
ScienzaMagia <47404591+ScienzaMagia@users.noreply.github.com>
2022-02-08 15:18:01 -0500

    1. Added conversion to PDF\n2. Added parsing table of contents for bulk downloading.

--- webtoonRip.py
+++ webtoonRip.py
@@ -12,16 +12,71 @@ class webtoonRip:
 
     def main(self):
         url = sys.argv[1]
+        
+        #determines if url is a table of contents, a specific issue, or neither
+        
+        if url.find("page=") != -1:
+            print ("Title Page")
+            issues = self.findIssues(url,[])
+            i = len(issues) -1
+            while i >= 0:
+                self.issueGrab(issues[i])
+                i = i-1
+            
+        elif url.find("episode_no=") != -1:
+            print ("Episode")
+            self.issueGrab(url)
+        else:
+            print ("Invalid link")
+        
+        
+        
+        #self.findIssues(url,[])
+        #self.issueGrab(url)
+       
+        
+        
+    #Crawls through the episode lists to grab a complete list of issues.
+    def findIssues(self, url, issues):
+        #finds page number from url
+        pageIndex = int(url.find("page="))
+        pageNumber = int(url[pageIndex+5:len(url)])
+        strippedUrl = url[0:pageIndex+5]
+        
+        #pulls and parses page contents
+        page = requests.get(url, headers={'referer': 'webtoons.com'})
+        pageHtml = BeautifulSoup(page.content, 'html.parser')
+        
+        #finds list of episodes on page
+        i = 0
+        issuesOnPage = pageHtml.find('ul', id = '_listUl')
+        for child in issuesOnPage.children:  
+            if (str(child)) != "\n":
+                issues.append(str(child.a['href']))
+                file1 = open("EpisodeList.txt", "a")  # append mode
+                file1.write(str(child.a['href']) + " \n")
+                file1.close()
+                if child['data-episode-no'] == "1":
+                    return issues
+                i = i+1
+                
+
+        issues = self.findIssues(strippedUrl + str(pageNumber + 1), issues)
+        return issues
+        
+        
+    def issueGrab(self, url):
         response = requests.get(url, headers={'referer': 'webtoons.com'})
         rawHtml = BeautifulSoup(response.content, 'html.parser')
-
         title = rawHtml.title.contents[0]
-        #print(title)
-        title = re.sub(r'[^a-zA-Z0-9_ ]','', title)
-        if not os.path.exists(title):
-            os.mkdir(title)
-        
         print(title)
+        title = re.sub(r'[^a-zA-Z0-9_ ]','', title)
+        directory = "rip/" + title + "/"
+        if not os.path.exists(directory + "raw/"):
+            os.makedirs(directory + "raw/")
+
+			
+        
         imagelist = rawHtml.find("div", id = "_imageList").find_all("img")
         
         mergedImage = Image.new('RGB', (0,0), color=0)
@@ -29,17 +84,24 @@ class webtoonRip:
         while i < len(imagelist):
             #print(i)
             #print (imagelist[i]['data-url'])
-            self.imageGrab(title, imagelist[i]['data-url'], i)
-            image2Merge = Image.open(title + "/" + str(i+1) + ".jpg")
+            self.imageGrab(directory, imagelist[i]['data-url'], i)
+            image2Merge = Image.open(directory + "/raw/" + str(i+1).zfill(4) + ".jpg")
             mergedImage = self.imageStitch(mergedImage, image2Merge)
             image2Merge.close()
+            print(title + " Image: "  + str(i+1).zfill(4) + "/" +  str(len(imagelist)).zfill(4), end = "\r")
             i = i+1
-
-        mergedImage.save("merged.jpg", quality=100)
+        
+        mergedImage.save(directory + title +".jpg", quality=100)
+        
+        with open(directory + title + ".pdf","wb") as f:
+            f.write(img2pdf.convert(directory + title + ".jpg"))
+      
+      
+        print("\nComplete")
 
     # Gets image at "url" and saves it as a .jpg to "directory"
     def imageGrab(self, directory,  url, pageNumber):
-        with open(directory + "/" + str(pageNumber+1) + '.jpg', 'wb') as f:
+        with open(directory + "raw/" + str(pageNumber+1).zfill(4) + '.jpg', 'wb') as f:
             f.write(requests.get(url, headers={'referer': 'webtoons.com'}).content)
         f.close()
 
